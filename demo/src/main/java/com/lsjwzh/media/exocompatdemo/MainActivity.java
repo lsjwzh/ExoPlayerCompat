a6178fa412f02a1c4b22aa1d6955d2453d61abe3
panwenye <lsjwzh@163.com>
2014-08-25 16:59:38 +0800

    update demo

--- demo/src/main/java/com/lsjwzh/media/exocompatdemo/MainActivity.java
+++ demo/src/main/java/com/lsjwzh/media/exocompatdemo/MainActivity.java
@@ -1,19 +1,100 @@
 package com.lsjwzh.media.exocompatdemo;
 
+import android.net.Uri;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.SurfaceHolder;
+import android.view.View;
+
+import com.google.android.exoplayer.ExoPlaybackException;
+import com.google.android.exoplayer.ExoPlayer;
+import com.google.android.exoplayer.VideoSurfaceView;
+import com.lsjwzh.media.exoplayercompat.MediaPlayerCompat;
+import com.lsjwzh.media.exoplayercompat.exo.DefaultRendererBuilder;
+import com.lsjwzh.media.exoplayercompat.exo.ExoPlayerCompatImpl;
+import com.lsjwzh.media.exoplayercompat.exo.ExoPlayerWrapper;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends ActionBarActivity implements SurfaceHolder.Callback,
+        ExoPlayer.Listener{
+
+    VideoSurfaceView surface_view;
+
+    private MediaPlayerCompat player;
+    private boolean playerNeedsPrepare;
+
+    private boolean autoPlay = true;
+    private int playerPosition;
+    private boolean enableBackgroundAudio = false;
+    private EventLogger eventLogger;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        surface_view = (VideoSurfaceView)findViewById(R.id.surface_view);
+        surface_view.getHolder().addCallback(this);
     }
 
+    @Override
+    public void onResume() {
+        super.onResume();
+        preparePlayer();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        if (!enableBackgroundAudio) {
+            releasePlayer();
+        } else {
+            player.setDisplay(null);
+        }
+    }
+
+
+    private void preparePlayer() {
+        if (player == null) {
+            player = new ExoPlayerCompatImpl();
+            //new ExoPlayerWrapper(new DefaultRendererBuilder(this, Uri.parse("http://mofunsky-video.qiniudn.com/90/193/2014082012541850633.webm")));
+//            player.addListener(this);
+            player.setDataSource(this,"http://mofunsky-video.qiniudn.com/90/193/2014082012541850633.webm");
+            player.seekTo(playerPosition);
+            playerNeedsPrepare = true;
+//            mediaController.setMediaPlayer(player.getPlayerControl());
+//            mediaController.setEnabled(true);
+            eventLogger = new EventLogger();
+            eventLogger.startSession();
+        }
+        if (playerNeedsPrepare) {
+            player.prepare();
+            playerNeedsPrepare = false;
+
+        }
+        player.setDisplay(surface_view.getHolder());
+        maybeStartPlayback();
+    }
+
+    private void maybeStartPlayback() {
+        if (autoPlay && (surface_view.getHolder().getSurface().isValid())) {
+            player.start();
+            autoPlay = false;
+        }
+    }
+
+    private void releasePlayer() {
+        if (player != null) {
+            playerPosition = (int) player.getCurrentPosition();
+            player.release();
+            player = null;
+            eventLogger.endSession();
+            eventLogger = null;
+        }
+    }
+
+    // DemoPlayer.Listener implementation
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -32,5 +113,41 @@ public class MainActivity extends ActionBarActivity {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+    // SurfaceHolder.Callback implementation
+
+    @Override
+    public void surfaceCreated(SurfaceHolder holder) {
+        if (player != null) {
+            player.setDisplay(holder);
+            maybeStartPlayback();
+        }
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+        // Do nothing.
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder holder) {
+        if (player != null) {
+            player.setDisplay(null);
+        }
+    }
+
+    @Override
+    public void onPlayerStateChanged(boolean playWhenReady, int playbackState) {
+
+    }
+
+    @Override
+    public void onPlayWhenReadyCommitted() {
+
+    }
+
+    @Override
+    public void onPlayerError(ExoPlaybackException error) {
+
     }
 }
